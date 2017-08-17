f2ab25ba851477fc2c6631a8fd660774f8d59ce7
Jens Bendisposto <jens@bendisposto.de>
2017-08-17 16:02:29 +0200

    desciption / url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject es-auas/es-access "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Connector to our eventstore, probably you don't want this lib"
+  :url "https://github.com/cs-auas/es-connector"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
